f21fd3e475545d7f8fea7e2554f1560494b8023e
ekalinov <106105805+ekalinov@users.noreply.github.com>
2023-07-24 14:18:29 +0300

    GitIgnore Test

---
+++ BikingBuddy/BikingBuddy.Data/Migrations/20230724104312_IsDeletedAddToBike.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikingBuddy.Data.Migrations
+{
+    public partial class IsDeletedAddToBike : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bikes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bikes");
+        }
+    }
+}
