95b0a232aa06d51f8ee63d16d05b248519129623
ekalinov <106105805+ekalinov@users.noreply.github.com>
2023-08-01 15:30:46 +0300

    Team Contoller and Index View Created

---
+++ BikingBuddy/BikingBuddy.Web/Areas/Administration/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BikingBuddy.Web.Areas.Administration.Controllers
+{
+	public class TeamController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
