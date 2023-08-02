89ecde4de80139579d23d0976c3df5fad985e8f1
ekalinov <106105805+ekalinov@users.noreply.github.com>
2023-08-03 00:49:58 +0300

    Gallery Photo Model added

---
+++ BikingBuddy/BikingBuddy.Web.Models/GalleryPhotoModel.cs
@@ -0,0 +1,8 @@
+namespace BikingBuddy.Web.Models;
+
+public class GalleryPhotoModel
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string URL { get; set; } = null!;
+}
