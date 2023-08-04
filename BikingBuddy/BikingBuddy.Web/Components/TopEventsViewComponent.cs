a3716269a6941aeed3c368c4cd6b5fa3394cece3
ekalinov <106105805+ekalinov@users.noreply.github.com>
2023-08-04 13:58:11 +0300

    TopComponents refoctor to work also for similar events

--- BikingBuddy/BikingBuddy.Web/Components/TopEventsViewComponent.cs
+++ BikingBuddy/BikingBuddy.Web/Components/TopEventsViewComponent.cs
@@ -1,26 +1,32 @@
 using BikingBuddy.Services.Contracts;
+using BikingBuddy.Web.Models.Event;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikingBuddy.Web.Components;
 
 public class TopEventsViewComponent : ViewComponent
 {
-    
     private readonly IEventService eventService;
-    
+
     public TopEventsViewComponent(IEventService _eventService)
     {
-
         eventService = _eventService;
     }
-    
-    
-    public async Task<IViewComponentResult> InvokeAsync()
+
+
+    public async Task<IViewComponentResult> InvokeAsync(string? eventId)
     {
-        var topEvents = await eventService.GetNewestEventsAsync();
+        IList<EventMiniViewModel> model;
+        if (eventId != null)
+        {
+            model = await eventService.GetNewestEventsAsync(eventId);
+        }
+        else
+        {
+            model = await eventService.GetNewestEventsAsync();
+        }
 
 
-        return View(topEvents);
+        return View(model);
     }
-    
 }
