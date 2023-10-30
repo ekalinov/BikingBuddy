9590a3de07d1a03dd8ea79ffe438cf56cfb63543
ekalinov <106105805+ekalinov@users.noreply.github.com>
2023-10-30 15:23:08 +0200

    Event Details Js File Change> Add check if Event has uploaded track

--- BikingBuddy/BikingBuddy.Web/wwwroot/js/GpxTrackAndElevationProfileScripts.js
+++ BikingBuddy/BikingBuddy.Web/wwwroot/js/GpxTrackAndElevationProfileScripts.js
@@ -1,7 +1,69 @@
-﻿var gpxFile = document.getElementById('gpxGpx').innerText;
+﻿
+// Meeting point map
 
+var mp_lat = parseFloat(document.getElementById('meeting_point_lat').innerText);
+var mp_long = parseFloat(document.getElementById('meeting_point_long').innerText);
+
+var mapOptions = {
+    center: [mp_lat, mp_long],
+    zoom: 13,
+    zoomControl: false
+}
+
+var map_meetingPoint = L.map('map_meetingPoint', mapOptions);
+
+map_meetingPoint.scrollWheelZoom.disable();
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map_meetingPoint)
+
+
+let marker = null;
+
+marker = L.marker([mp_lat, mp_long]).addTo(map_meetingPoint);
+
+
+// Meeting point map Modal
+
+var mapOptions_modal = {
+    center: [mp_lat, mp_long],
+    zoom: 13,
+    zoomControl: false
+}
+var map_meetingPoint_modal = L.map('map_meetingPoint_modal1',mapOptions_modal);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map_meetingPoint_modal)
+
+document.getElementById('map_meetingPoint').addEventListener("click", function () {
+    marker = L.marker([mp_lat, mp_long]).addTo(map_meetingPoint_modal);
+
+
+    marker.bindPopup("<b>Meeting point</b><br>Here! ").openPopup();
+
+    document.getElementById('Modal_MeetingPoint').style.display = 'block';
+    setTimeout(function () {
+        map_meetingPoint_modal.invalidateSize();
+    }, 100);
+});
+
+
+
+
+
+//GPX Track Map and Chart
+
+var gpxFile = document.getElementById('gpxGpx');
+if (gpxFile!=null){
+    
+var gpxContent = gpxFile.innerText;
+ 
 var gpx = new gpxParser(); //Create gpxParser Object
-gpx.parse(gpxFile)
+gpx.parse(gpxContent)
 
 var points = gpx.tracks[0].points;
 
@@ -52,13 +114,13 @@ new Chart(ctx, {
 
 var track = gpx.calcElevation(points);
 
-document.getElementById('dist').textContent = eleProfile.total;
+document.getElementById('dist').textContent = (eleProfile.total/1000).toFixed(3);
 
-document.getElementById('pos').textContent = track.pos;
+document.getElementById('pos').textContent = Math.floor(track.pos);
 
-document.getElementById('max').textContent = track.max;
+document.getElementById('max').textContent =  Math.floor(track.max);
 
-document.getElementById('min').textContent = track.min;
+document.getElementById('min').textContent =  Math.floor(track.min);
 
 // Track info map
 
@@ -70,7 +132,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map)
 
 
-new L.GPX(gpxFile, {async: true}).on('loaded', function (e) {
+new L.GPX(gpxContent, {async: true}).on('loaded', function (e) {
     map.fitBounds(e.target.getBounds());
 }).addTo(map);
 
@@ -93,59 +155,4 @@ map.on('blur', () => {
     map.scrollWheelZoom.disable();
 });
 
-
-// Meeting point map
-
-var mp_lat = parseFloat(document.getElementById('meeting_point_lat').innerText);
-var mp_long = parseFloat(document.getElementById('meeting_point_long').innerText);
-
-var mapOptions = {
-    center: [mp_lat, mp_long],
-    zoom: 13,
-    zoomControl: false
 }
-
-var map_meetingPoint = L.map('map_meetingPoint', mapOptions);
-
-map_meetingPoint.scrollWheelZoom.disable();
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map_meetingPoint)
-
-
-let marker = null;
-
-marker = L.marker([mp_lat, mp_long]).addTo(map_meetingPoint);
-
-
-// Meeting point map Modal
-
-
-
-var mapOptions_modal = {
-    center: [mp_lat, mp_long],
-    zoom: 13,
-    zoomControl: false
-}
-var map_meetingPoint_modal = L.map('map_meetingPoint_modal1',mapOptions_modal);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map_meetingPoint_modal)
-
-document.getElementById('map_meetingPoint').addEventListener("click", function () {
-marker = L.marker([mp_lat, mp_long]).addTo(map_meetingPoint_modal);
-
-
-marker.bindPopup("<b>Meeting point</b><br>Here! ").openPopup();
-
-    document.getElementById('Modal_MeetingPoint').style.display = 'block';
-    setTimeout(function () {
-        map_meetingPoint_modal.invalidateSize();
-    }, 100);
-});
- 
-
